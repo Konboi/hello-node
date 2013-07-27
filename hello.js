dd26b45df5230594bd69589b1aac1932f8035f77
Konboi <ryosuke.yabuki@gmail.com>
2013-07-27 21:18:04 +0900

    Added show request url

--- hello.js
+++ hello.js
@@ -11,7 +11,7 @@ var http = require("http");
  */
 http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("hello, world! \n");
+    res.end("hello, world! :URL:" + req.url + "\n");
 }).listen(PORT, IP_ADDRESS, function () {
     console.log("server runnring at http://" + IP_ADDRESS + ":" + PORT + "/");
     console.log("Server Stop: Ctrl+c");
