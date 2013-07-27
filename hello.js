d71c1d7886ccfb3a3c29bdf9e540880a91ee4422
Konboi <ryosuke.yabuki@gmail.com>
2013-07-27 21:06:09 +0900

    Added log message

--- hello.js
+++ hello.js
@@ -2,4 +2,7 @@ var http = require("http");
 http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.end("hello, world! \n");
-}).listen(1337, "localhost");
+}).listen(1337, "localhost", function () {
+    console.log("server runnring at http://localhost:1337");
+    console.log("Server Stop: Ctrl+c");
+});
