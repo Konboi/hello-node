df10f930dc25a433df72222e0d68733ac798bc37
Konboi <ryosuke.yabuki@gmail.com>
2013-07-27 21:14:58 +0900

    Set Ip and Host for variable

--- hello.js
+++ hello.js
@@ -1,8 +1,12 @@
+var IP_ADDRESS = "localhost";
+var PORT = 1337;
+
 var http = require("http");
 http.createServer(function (req, res) {
     res.writeHead(200, {"Content-Type": "text/plain"});
     res.end("hello, world! \n");
-}).listen(1337, "localhost", function () {
-    console.log("server runnring at http://localhost:1337");
+}).listen(PORT, IP_ADDRESS, function () {
+    console.log("server runnring at http://" + IP_ADDRESS + ":" + PORT + "/");
     console.log("Server Stop: Ctrl+c");
+
 });
